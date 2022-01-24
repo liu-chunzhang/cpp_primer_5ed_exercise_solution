bf2cbe62eeb3c4d1eacd8af8c4bc0c9ed2a65074
Liu-Chunzhang <lkz9808@163.com>
2022-01-24 22:52:14 +0800

    none

--- 第01章 开始/1.01 编写一个简单的C++程序/1.1 编写一个简单的C++程序/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-using namespace std;
-int main() {
-    cout << "Hello, C++ world!" << endl;
-    return 0;
-}
--- 第01章 开始/1.07 练习集合/练习 1.13/练习 1.13(2)/main.cpp
+++
@@ -1,9 +0,0 @@
-// 本程序用于验证读者在练习1.13中的想法，并在必要的地方予以说明：使用for循环按递减顺序打印10到0之间的整数
-#include <iostream>
-using namespace std;
-
-int main() {
-    for( int i = 10 ; i >= 0 ; --i )
-        cout << i << endl;
-    return 0;
-}
